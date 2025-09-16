1d5e32b7868ab689d13519efffec99b0d60b70e5
DANILO CESAR DA SILVA <danilo.silva1@alunos.sc.senac.br>
2025-09-15 21:51:26 -0300

    adding class

---
+++ Preparo.java
@@ -0,0 +1,13 @@
+public class Preparo {
+    int idpreparo;
+    String modo_preparo;
+    String urlvideo;
+    String tempopreparo;
+
+    public Preparo(int idpreparo, String modo_preparo, String urlvideo, String tempopreparo){
+        this.idpreparo = idpreparo;
+        this.modo_preparo = modo_preparo;
+        this.urlvideo = urlvideo;
+        this.tempopreparo = tempopreparo;
+    }
+}
---
+++ ReceitaIngrediente.java
@@ -0,0 +1,7 @@
+public class ReceitaIngrediente {
+    public float qtd;
+
+    public ReceitaIngrediente(float qtd) {
+        this.qtd = qtd;
+    };
+}
