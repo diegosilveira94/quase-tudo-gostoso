c7313bfe8d5a84f8d13924ba773be930775cc6e5
DANILO CESAR DA SILVA <danilocesardasilva53@gmail.com>
2025-10-27 19:32:51 -0300

    making API

--- src/main/java/br/com/quasetudogostoso/App.java
+++ src/main/java/br/com/quasetudogostoso/App.java
@@ -1,8 +1,10 @@
 package src.main.java.br.com.quasetudogostoso;
 
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import src.main.java.br.com.quasetudogostoso.controller.ReceitaController;
 import src.main.java.br.com.quasetudogostoso.service.ReceitaService;
+import src.main.java.br.com.quasetudogostoso.service.UsuarioService;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -14,9 +16,14 @@ public class App {
         HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
 
         ReceitaService receitaService = new ReceitaService();
+        UsuarioService usuarioService = new UsuarioService();
+        service.IngredienteService ingredienteService = new service.IngredienteService();
+
         ReceitaController receitaController = new ReceitaController(receitaService);
 
         server.createContext("/receitas", receitaController);
+        server.createContext("/usuario", (HttpHandler) usuarioService);
+        server.createContext("/ingrediente", (HttpHandler) ingredienteService);
         server.setExecutor(null);
         server.start();
 
